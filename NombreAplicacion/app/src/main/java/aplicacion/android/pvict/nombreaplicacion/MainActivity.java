ff9b406be0493a8c742b475d44e494eb38a31e8e
Victor Parra <pvictor3@gmail.com>
2017-08-03 11:31:15 -0500

    My first commit

---
+++ NombreAplicacion/app/src/main/java/aplicacion/android/pvict/nombreaplicacion/MainActivity.java
@@ -0,0 +1,13 @@
+package aplicacion.android.pvict.nombreaplicacion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
