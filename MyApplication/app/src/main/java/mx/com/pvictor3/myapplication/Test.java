2af36e104ade0133921d6f7c8e7474dcde4f32f0
Victor Parra <pvictor3@gmail.com>
2017-08-27 20:50:30 -0500

    primer commit desde androidStudio

---
+++ MyApplication/app/src/main/java/mx/com/pvictor3/myapplication/Test.java
@@ -0,0 +1,15 @@
+package mx.com.pvictor3.myapplication;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by pvict on 27/08/2017.
+ */
+
+public class Test {
+
+    public static void myToast(Context context){
+        Toast.makeText(context, "Hola amigos!!!",Toast.LENGTH_LONG).show();
+    }
+}
--- NombreAplicacion/app/src/main/java/aplicacion/android/pvict/nombreaplicacion/MainActivity.java
+++ NombreAplicacion/app/src/main/java/aplicacion/android/pvict/nombreaplicacion/MainActivity.java
@@ -1,13 +1,32 @@
 package aplicacion.android.pvict.nombreaplicacion;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "Hola desde el curso Aprendiendo Android", Toast.LENGTH_LONG).show();
+    }
+
+    /*
+    Called when the user clicks the Send button
+     */
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
