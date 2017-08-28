4ddde34513df06dcb7ec4c5489885baeb038a900
Victor Parra <pvictor3@gmail.com>
2017-08-27 20:57:41 -0500

    primer commit desde androidStudio

---
+++ MyApplication/app/src/main/java/mx/com/pvictor3/myapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package mx.com.pvictor3.myapplication;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
